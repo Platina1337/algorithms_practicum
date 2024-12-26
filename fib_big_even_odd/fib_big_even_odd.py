2376d7e3c0a002d23f7706ebe9b9d865f354b6aa
Platina1337 <116428617+Platina1337@users.noreply.github.com>
2024-12-26 21:48:19 +0100

    Update fib_big_even_odd.py

--- fib_big_even_odd/fib_big_even_odd.py
+++ fib_big_even_odd/fib_big_even_odd.py
@@ -1,10 +1,15 @@
 def fib_big_even_odd(n):
-    if n % 2 == 0:
+    if n <= 0:
         return "even"
-    else:
-        return "odd"
+    
+
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, (a + b) % 10
+    
+    return "even" if b % 2 == 0 else "odd"
 
 if __name__ == "__main__":
     n = 841645
     result = fib_big_even_odd(n)
-    print(f"fib({n}) is {result}")
+    print(result)  
