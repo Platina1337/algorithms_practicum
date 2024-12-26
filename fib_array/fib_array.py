6a3db09621f7785037c1aad4c5fddfdf0ee3d837
Platina1337 <116428617+Platina1337@users.noreply.github.com>
2024-12-26 21:48:45 +0100

    Update fib_array.py

--- fib_array/fib_array.py
+++ fib_array/fib_array.py
@@ -1,4 +1,9 @@
 def fib_array(n):
+    if n < 0:
+        return []
+    if n == 0:
+        return [0]
+        
     fib_series = [0, 1]
     for i in range(2, n + 1):
         fib_series.append(fib_series[-1] + fib_series[-2])
@@ -7,4 +12,4 @@ def fib_array(n):
 if __name__ == "__main__":
     n = 8
     result = fib_array(n)
-    print(f"Fibonacci series up to {n}: {result}")
+    print(result)
