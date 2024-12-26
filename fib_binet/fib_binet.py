f85b8bd53a62b7dc546c7ae5ad71e627c2cb4314
Platina1337 <116428617+Platina1337@users.noreply.github.com>
2024-12-26 21:49:28 +0100

    Update fib_binet.py

--- fib_binet/fib_binet.py
+++ fib_binet/fib_binet.py
@@ -1,6 +1,8 @@
 import math
 
 def fib_binet(n):
+    if n < 0:
+        return 0
     sqrt_5 = math.sqrt(5)
     phi = (1 + sqrt_5) / 2
     psi = (1 - sqrt_5) / 2
@@ -9,4 +11,4 @@ def fib_binet(n):
 
 if __name__ == "__main__":
     n = 32
-    print(f"fib({n}) using Binet's formula: {fib_binet(n)}")
+    print(fib_binet(n))
